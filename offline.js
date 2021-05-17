896e999684e95d3dcce33280d1fad46764136a29
Poishiche <damien.belghit@yahoo.fr>
2021-05-17 18:57:13 +0200

    Chat WSS

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1621246432,
+	"version": 1621270518,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -413,12 +413,12 @@
 		"8-bit_madness-webfont.ttf",
 		"8-bit_madness-webfont.woff",
 		"eight-bit_madnessregular.css",
-		"8-bit_hudregular.css",
-		"8-bit-hud-webfont.woff",
-		"8-bit-hud-webfont.woff2",
 		"nokia_cellphone_fcsmall.css",
 		"nokiafc22-webfont.woff",
 		"nokiafc22-webfont.woff2",
+		"8-bit_fortressregular.css",
+		"8-bit_fortress-webfont.woff",
+		"8-bit_fortress-webfont.woff2",
 		"loading-logo.png",
 		"Photon-Javascript_SDK.min.js",
 		"Photon-Javascript_SDK.min.js",
